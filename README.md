e74140069109944bd8d64d38c3238fee95fc120d
AlaAlmuhsen <Ala.Almuhsen@gmail.com>
2023-05-22 12:55:20 +0300

    Adding Projects Files

--- README.md
+++ README.md
@@ -1,2 +1,11 @@
 # School-Regestration-Platform
  Project Number 5 For Orange Coding Academy about registration platform for school 
+
+
+This project is done by :
+
+1- Ala'a Almuhsen 
+3- Muhammad Alfreijat
+2- Dania Atieh
+4- Mohammad Abdelhadi
+5- Mohammad Saadeh
---
+++ js/main.js
@@ -0,0 +1,17 @@
+/* Start Main Page */
+/* End Main Page */
+
+/* Start signin Page */
+/* End signin Page */
+
+/* Start signUp Page */
+/* End signUP Page */
+
+/* Start E-test Page */
+/* End E-test Page */
+
+/* Start M_test Page */
+/* End M-test Page */
+
+/* Start infoform Page */
+/* End infoform Page */
