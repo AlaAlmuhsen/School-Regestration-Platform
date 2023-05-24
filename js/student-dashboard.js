48a128ab32ad271a02c657ffda2326e0251dce9c
dania atieh <dania.a.atieh@gmail.com>
2023-05-24 10:42:50 +0300

    Dania's updated Footer

--- js/student-dashboard.js
+++ js/student-dashboard.js
@@ -4,3 +4,17 @@ var signout = document.getElementById("sign_out");
 signout.onclick = function () {
 
 };
+
+function changeSignOutIcon(){
+    if (screen.width < 576){
+        signout.innerHTML=`<img style="width: 100%; height: 60%; filter: invert(100%);" src="img/logout.svg" alt="signOut">`;
+
+    }else{
+        signout.innerHTML="Sign Out";
+
+    }
+
+}
+window.addEventListener("resize", function(){
+    changeSignOutIcon()
+});
