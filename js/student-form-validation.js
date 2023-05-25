dcb8ae3344f4a4b94d7c5e47ddb909efc196071b
unknown <Mohammad-Abdelhadi>
2023-05-25 22:02:45 +0300

    done form by mohammad abdelhadi

--- js/student-form-validation.js
+++ js/student-form-validation.js
@@ -4,6 +4,22 @@ document.getElementById("birthday").max = currentDate;
 (() => {
   "use strict";
 
+  // Check if the form has already been submitted
+  if (localStorage.getItem("formSubmitted") === "true") {
+    const form = document.querySelector(".needs-validation");
+    let info = document.querySelector(".info");
+    info.style.display = "none";
+    form.innerHTML = `
+      <h1 style="color: green; text-align: center;">Form Already Submitted. Thank You!</h1>
+      <div class="rtn__button__container d-flex w-100 justify-content-center">
+        <a href="student-dashboard.html">
+          <button class="return__button my-3">RETURN</button>
+        </a>
+      </div>
+    `;
+    
+  }
+
   // Fetch all the forms we want to apply custom Bootstrap validation styles to
   const forms = document.querySelectorAll(".needs-validation");
 
@@ -26,13 +42,36 @@ document.getElementById("birthday").max = currentDate;
           Array.from(fields).forEach((field) => {
             formData[field.id] = field.value;
           });
-          form.innerHTML = `<h1> Thank Your For Enter Your Infromation . </h1>
-          `;
-          form.style.color = "green";
+
           console.log(formData);
+          // Store the form data in local storage
+          localStorage.setItem("formData", JSON.stringify(formData));
+          // Mark the form as submitted
+          localStorage.setItem("formSubmitted", "true");
+
+          form.innerHTML = `
+            <h1 style="color: green; text-align: center;">Form Submitted. Thank You!</h1>
+            <div class="rtn__button__container d-flex w-100 justify-content-center">
+              <a href="student-dashboard.html">
+                <button class="return__button my-3">RETURN</button>
+              </a>
+            </div>
+          `;
+          let info = document.querySelector(".info");
+          info.style.display = "none";
+          const returnButton = document.querySelector(".return__button");
+          returnButton.addEventListener("click", function () {
+            window.location.href = "student-dashboard.html";
+          });
         }
 
         form.classList.add("was-validated");
+        // Bootstrap Classs -> was-validated
+        // It adds a green checkmark icon next to valid form controls.
+        // It adds a red exclamation mark icon next to invalid form controls.
+        // It applies a green border color to valid form controls.
+        // It applies a red border color to invalid form controls.
+        // It displays validation feedback messages below the form controls.
       },
       false
     );
